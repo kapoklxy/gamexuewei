8988b6941913ead7d65a8b3f63ded0ce5ca63afb
kapoklxy <kapok1982lxy@gmail.com>
2025-06-07 21:20:24 +0800

    Add files via upload
    重大改版。增加固定画框

---
+++ gameXuewei/acupoints.js
@@ -0,0 +1,16 @@
+const acupoints = [
+    {
+        name: '少商',
+        x: 82,
+        y: 1073,
+        description: '在手拇指末节桡侧，距指甲角0.1寸。'
+    },
+    {
+        name: '太渊',
+        x: 206,
+        y: 1051,
+        description: '在腕掌侧横纹桡侧，桡动脉搏动处。'
+    }
+];
+
+export default acupoints; 
